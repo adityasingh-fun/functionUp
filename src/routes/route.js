6ad737bfb5dae36bfca1ed4b287569a5b3e8d1cb
adityasingh-fun <chaudharyaditya41@gmail.com>
2023-04-25 23:17:04 +0530

    assignment on path and query params

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,63 @@ const { route } = require('express/lib/application');
 const router = express.Router();
 
 
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+// WRITE A POST API TO THE ABOVE
+
+
+// take this as sample for array of persons:
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]
+
+
+router.post('/get-person',function(req,res){
+    let age = req.query.age
+    let arr = []
+    for(let i=0;i<persons.length;i++){
+        if(persons[i].age >= age){
+            persons[i].votingStatus = true
+            arr.push(persons[i])
+        }
+    }
+    console.log(age)
+    res.send( {data : arr, status : true})
+})
+
+
+
+
+
+
+/*
+
 router.post('/post-req',function(req,res){
     // let id = req.body.user_id
     // let pwd = req.body.password
@@ -184,3 +241,5 @@ router.get('/students/:studentName', function(req, res) {
 
 
 module.exports = router;
+
+*/
