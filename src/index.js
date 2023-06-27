e8e44a56376bed053af8bbf158e131d783c27b27
adityasingh-fun <chaudharyaditya41@gmail.com>
2023-06-28 00:33:29 +0530

    updated s3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,62 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-const route = require('./routes/route.js');
-
+const route = require('./routes/route');
 const app = express();
 
-const multer= require("multer");
-const { AppConfig } = require('aws-sdk');
+const multer = require('multer');
+const {AppConfig} = require('aws-sdk');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use( multer().any())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(multer().any());
 
-app.use('/', route);
+app.use('/',route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express app running at port "+ (process.env.PORT || 3000))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// var bodyParser = require('body-parser');
+
+// const route = require('./routes/route.js');
+
+// const app = express();
+
+// const multer= require("multer");
+// const { AppConfig } = require('aws-sdk');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use( multer().any())
+
+// app.use('/', route);
+
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log('Express app running on port ' + (process.env.PORT || 3000))
+// });
